3bd89ae63d68ea31e4ec93fb099131438fd71c1c
habib_wahid <habib11wahid@gmail.com>
2024-01-28 17:25:59 +0600

    introducing rabbit-mq for sending notification

--- processor/src/main/java/com/loofi/notification/processor/configs/NotificationAppConfig.java
+++ processor/src/main/java/com/loofi/notification/processor/configs/NotificationAppConfig.java
@@ -2,13 +2,13 @@ package com.loofi.notification.processor.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class NotificationAppConfig {
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+//  @Bean
+//  public PasswordEncoder passwordEncoder() {
+//    return new BCryptPasswordEncoder();
+//  }
 }
--- processor/src/main/java/com/loofi/notification/processor/services/NotificationService.java
+++ processor/src/main/java/com/loofi/notification/processor/services/NotificationService.java
@@ -3,6 +3,8 @@ package com.loofi.notification.processor.services;
 import com.loofi.notification.common.entities.Notification;
 import com.loofi.notification.processor.dtos.NotificationDTO;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,15 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
+
+  @Value("${rabbitmq.exchange.name}")
+  private String exchange;
+  @Value("${rabbitmq.routing.key}")
+  private String routingKey;
+
   private final NotificationProcessor notificationProcessor;
+  private final RabbitTemplate rabbitTemplate;
 
   public void processAndPublish(NotificationDTO notificationDTO) {
     log.info("Notification request.");
     Notification notification = notificationProcessor.processNotification(notificationDTO);
     log.info("Notification. ID:{}",notification.getId());
-    // todo publish message queue with id
-    // consumer fetch data from message queue
-    // and send notification corresponding channel ( SMS thru telco, email thru email network, Push Notification
-
+    rabbitTemplate.convertAndSend(exchange,routingKey, notification.getId().toString());
   }
 }
